6bfb8d69fb700fe9152b164d692ae8c97c353327
Eric Mittelhammer <code@ericmittelhammer.com>
2019-08-30 10:11:21 -0400

    Repo has been moved

--- build_events/README.md
+++
@@ -1,2 +0,0 @@
-# Build Event Scripts
-Scripts that are executed as part of the CodeBuild process.  Configured by the [buildspecs](../buildspecs).
--- build_events/tag-source.js
+++
@@ -1,9 +0,0 @@
-// used by CodeBuild to write the build information to a file that can then be picked up
-// by the app and attached to New Relic custom events.
-
-const buildInfo = {
-    commitSha: process.env['CODEBUILD_RESOLVED_SOURCE_VERSION'],
-    buildId: process.env['CODEBUILD_BUILD_ID']
-};
-
-process.stdout.write(JSON.stringify(buildInfo, null, 2) + '\n');
